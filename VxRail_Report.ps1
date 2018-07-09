a06586b6bb4e2bdfa29be214939c7d9b0481cc76
vmben <40983193+vmben@users.noreply.github.com>
2018-07-09 08:54:53 +0100

    Add files via upload

--- VxRail_Report.ps1
+++ VxRail_Report.ps1
@@ -1,19 +1,18 @@
 Import-Module VMware.VimAutomation.Core -ErrorAction:SilentlyContinue
 Import-Module VMware.VimAutomation.Storage -ErrorAction:SilentlyContinue
 
-#----------------------------------------------------------[Declarations]--------------------------------------------
+####################################################################################
 #Change the following as required
 ####################################################################################
  $XMLFile = "vsan.xml"				#Path to generated report
- $vCenter= Read-Host 'Please enter vCenter Server IP or FQDN'				#vCenter hostname or IP Address
+ $vCenter= Read-Host 'Please enter vCenter Server IP or FQDN'				
  $ReportVersion="0.1"
-
+ $dn=0 #disk counter
 
 #Initial XML Setup
 (@"
  <?xml version="1.0"?> 
- <Report_Version>$($Report_Version)</Report_Version>
- 
+ <Report_Version>$($ReportVersion)</Report_Version>
 "@) | Out-File $XMLFile
  
  
@@ -21,11 +20,11 @@ Import-Module VMware.VimAutomation.Storage -ErrorAction:SilentlyContinue
  
 try{
        Disconnect-VIServer -Confirm:$false -ErrorAction:SilentlyContinue
-       Connect-VIServer -Server $vCenter 
+       Connect-VIServer -Server $vCenter  -ErrorAction:SilentlyContinue
 }
 catch{
        Write-Host "Failed to connect to vCenter Server $vCenter"
-       exit #Exit script on error
+       exit 
 }
 
 
@@ -39,17 +38,17 @@ foreach ($vSAN in $vSANClusterList) {
             
             
         Write-Host "`tGathering configuration details from vSAN Cluster: $vSAN ..."
-        Write-Verbose -Message ((Get-Date -Format G) + "`tGathering claimed disks configuration...")
+        Write-Host ((Get-Date -Format G) + "`tGathering claimed disks configuration...")
         $vSAN = $vSAN | Get-VsanClusterConfiguration
         $vSanDiskGroups = Get-VsanDiskGroup -Cluster $vSAN.name
         $vSanDisks = Get-VsanDisk -vSANDiskGroup $vSanDiskGroups
         $numberDisks = $vSanDisks.Count
 
 
-		Write-Verbose -Message ((Get-Date -Format G) + "`tGathering disk group configuration...")
+		Write-Host ((Get-Date -Format G) + "`tGathering disk group configuration...")
         $numberDiskGroups = $vSanDiskGroups.Count
     
-        Write-Verbose -Message ((Get-Date -Format G) + "`tGathering cluster type Configuration...")
+        Write-Host ((Get-Date -Format G) + "`tGathering cluster type Configuration...")
         $magneticDiskCounter = ($vSanDisks | Where-Object {$_.IsSsd -eq $true}).Count
         if ($magneticDiskCounter -gt 0) {
             $clusterType = "Hybrid"
@@ -59,19 +58,19 @@ foreach ($vSAN in $vSANClusterList) {
         } 
 		
 		
-        Write-Verbose -Message ((Get-Date -Format G) + "`tGathering disk claim mode configuration...")
+        Write-Host ((Get-Date -Format G) + "`tGathering disk claim mode configuration...")
         $diskClaimMode = $vSAN.VsanDiskClaimMode
 
         
-        Write-Verbose -Message ((Get-Date -Format G) + "`tGathering deduplication & compression configuration...")
+        Write-Host((Get-Date -Format G) + "`tGathering deduplication & compression configuration...")
         $deduplicationCompression = $vSAN.SpaceEfficiencyEnabled
 
         
-        Write-Verbose -Message ((Get-Date -Format G) + "`tGathering stretched cluster configuration...")
+        Write-Host ((Get-Date -Format G) + "`tGathering stretched cluster configuration...")
         $stretchedCluster = $vSAN.StretchedClusterEnabled
             
         
-        Write-Verbose -Message ((Get-Date -Format G) + "`tGathering space usage data...")
+        Write-Host ((Get-Date -Format G) + "`tGathering space usage data...")
         $vSANCapacity = Get-VsanSpaceUsage -Cluster $vSAN.name
 
 		
@@ -84,12 +83,36 @@ foreach ($vSAN in $vSANClusterList) {
    <Cluster_Type>$(($clusterType))</Cluster_Type>
    <Disk_Claim_Mode>$($diskClaimMode)</Disk_Claim_Mode>
    <Stretched_Cluster>$($stretchedCluster)</Stretched_Cluster>
+   <Number_Disks>$($$numberDisks)</Number_Disks>
+   <Number_Diskgroups>$($numberDiskGroups)</Number_Diskgroups>
    </General_Info>
   <VSAN_$($vSAN.name)>
+  <Disks>
 "@) | Out-File $XMLFile -Append
 		
-		
+$vSanDisks | % {
+  
+(@"
+  <Disk_$dn>
+   <Number>$dn</Number>
+   <Name>$($_.Name)</Name>
+   <Health>$($_.ExtensionData.OperationalState)</Health>
+   <Vendor>$($_.ExtensionData.Vendor)</Vendor>
+   <Model>$($_.ExtensionData.Model)</Model>
+   <SSD>$($_.IsSsd)</SSD>
+   <Size_GB>$(($_.ExtensionData.Capacity.BlockSize * $_.ExtensionData.Capacity.Block) / (1024*1024*1024)))</Size_GB>
+   <Format_Ver>$($_.ExtensionData.VsanDiskInfo.FormatVersion)</Format_Ver>
+   <vSAN_UIID>$($_.ExtensionData.VsanDiskInfo.VsanUuid)</vSAN_UIID>
+   <ESXi_Host>$($_.VsanDiskGroup.vmhost.name)</ESXi_Host>
+   <Disk_Group>$($_.VsanDiskGroup)</Disk_Group>
+  </Disk>
+"@) | Out-File $vSANFile -Append
+  
+  $dn++
+}
 
+" </Disks>" | Out-File $vSANFile -Append
+#>
 
 
 }
@@ -105,41 +128,6 @@ foreach ($vSAN in $vSANClusterList) {
 
 <#
 
-#-------------------------------------------------------------------------------------
-#vSAN Health Info
-if ( $Clus.ExtensionData.ConfigIssue.ObjectName -eq "VSAN") {$vSANIssues = $Clus.ExtensionData.ConfigIssue[0].FullFormattedMessage}
 
-(@"
-  <Health>
-   <Issues>$vSANIssues</Issues>
-  </Health>
-  <Disks>
-"@) | Out-File $vSANFile -Append
 
-#-------------------------------------------------------------------------------------
-#vSAN Disks Info
-
-$vSanDisks | % {
-  
-(@"
-  <Disk_$dn>
-   <Number>$dn</Number>
-   <Name>$($_.Name)</Name>
-   <Health>$($_.ExtensionData.OperationalState)</Health>
-   <Vendor>$($_.ExtensionData.Vendor)</Vendor>
-   <Model>$($_.ExtensionData.Model)</Model>
-   <SSD>$($_.IsSsd)</SSD>
-   <Size_GB>$(($_.ExtensionData.Capacity.BlockSize * $_.ExtensionData.Capacity.Block) / (1024*1024*1024)))</Size_GB>
-   <Queue_Length>$($_.ExtensionData.QueueDepth)</Queue_Length>
-   <Format_Ver>$($_.ExtensionData.VsanDiskInfo.FormatVersion)</Format_Ver>
-   <vSAN_UIID>$($_.ExtensionData.VsanDiskInfo.VsanUuid)</vSAN_UIID>
-   <ESXi_Host>$($_.VsanDiskGroup.vmhost.name)</ESXi_Host>
-   <Disk_Group>$($_.VsanDiskGroup)</Disk_Group>
-  </Disk>
-"@) | Out-File $vSANFile -Append
-  
-  $dn++
-}
-
-" </Disks>" | Out-File $vSANFile -Append
 #>
